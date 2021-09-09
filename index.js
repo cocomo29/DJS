5d4e898484c47296c4ad445064cffbf00c6de0d5
cocomo <cocomo2907@gmail.com>
2021-09-09 17:21:55 +0500

    oh

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const Discord =  require('discord.js');
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({intents});
 const config = require('./config.json');
+const command =  require('./command')
 
 
 client.on('ready', () =>{
     console.log('The client is ready!')
+
+    command(client, 'ping', message=> {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(config.token)
